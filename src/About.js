171469b24a0325caa87696afed4e64b82d183378
Kyle Thomas <kylejwthomas@gmail.com>
2022-07-19 15:02:35 +0900

    finished digital business card

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function About() {
+    return(
+        <div className="text-section">
+            <h4>About</h4>
+            <p>
+            I am a fullstack developer with a particular interest in making apps that everyone can use. I and am always looking for new things to learn.</p>
+
+            <h4>Interests</h4>
+            <p>Snowboarding. Running. Karate. Travel. New Tech.</p>
+        </div>
+    )
+}
